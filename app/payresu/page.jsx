a9c04c6e3d843328c9537fa00dfe464f5e97e289
yoon2 <46823971@naver.com>
2025-12-22 11:52:11 +0900

    frontend

--- app/payresu/page.jsx
+++ app/payresu/page.jsx
@@ -1,5 +1,5 @@
 import { Suspense } from "react";
-import PayResultClient from "./PayResultClient";
+import PayResultClient from "./PayResultClient.jsx";
 
 export default function PayResultPage() {
   return (
