58b5c5055a721930cc1049989a119b9be30190e4
yoon2 <46823971@naver.com>
2025-12-22 11:43:19 +0900

    frontend

--- app/payResult/page.jsx
+++ app/payResult/page.jsx
@@ -1,22 +1,10 @@
-"use client";
-import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
+import PayResultClient from "./PayResultClient";
 
-export default function PayResult() {
-  const searchParams = useSearchParams();
-  const orderId = searchParams.get("orderId");
-  const status = searchParams.get("status");
-
+export default function PayResultPage() {
   return (
-    <div style={{ padding: "2rem" }}>
-      <h2>결제 결과 페이지</h2>
-      <p><b>주문번호:</b> {orderId}</p>
-      <p><b>결제 상태:</b> {status}</p>
-
-      {status === "success" ? (
-        <p style={{ color: "green" }}>✅ 결제가 정상적으로 완료되었습니다!</p>
-      ) : (
-        <p style={{ color: "red" }}>❌ 결제에 실패했습니다.</p>
-      )}
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <PayResultClient />
+    </Suspense>
   );
 }
